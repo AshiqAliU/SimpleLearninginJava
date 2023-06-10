27fd4f29116e7b1d312e67b604f22e73b28dfdd8
AshiqAliU <uashiqali@gmail.com>
2023-06-09 23:15:36 -0500

    Temporary Fix

--- src/main/java/InheritanceDemo/Dog.java
+++ src/main/java/InheritanceDemo/Dog.java
@@ -1,6 +1,6 @@
-package java.InheritanceDemo;
+package InheritanceDemo;
 
-public class Dog extends Animal{
+public class Dog extends Animal {
 
     Dog(String name){
         super(name); // call the constructor of the parent class - Animal class
